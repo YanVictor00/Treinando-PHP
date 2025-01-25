90a31c81fa71de2c6f8077ab5145b6e15916e78f
Yan Victor <yanvictor799@gmail.com>
2025-01-24 22:17:09 -0300

    rasc

--- rascunho.php
+++ rascunho.php
@@ -13,5 +13,8 @@ echo "Olá mudo!  \u{1F30E}";
 phpinfo();
 
 
-
+//Reprocessamento ,  Soma de itens, configuração.
+        date_default_timezone_set("America/Sao_Paulo");
+        echo "Hoje é dia " . date("d/M/Y");
+        echo " e a hora atual é " . date("g:i:s");
 ?>

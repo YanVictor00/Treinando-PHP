60d4df60270ff98f3b236ae4d608f4d7a801e926
Yan Victor <yanvictor799@gmail.com>
2025-01-28 21:42:15 -0300

    Variáveis e Constantes

--- rascunho.php
+++ rascunho.php
@@ -17,4 +17,13 @@ phpinfo();
         date_default_timezone_set("America/Sao_Paulo");
         echo "Hoje é dia " . date("d/M/Y");
         echo " e a hora atual é " . date("g:i:s");
+
+
+
+//Variáveis e Constantes
+        $name = "Yan";
+        $lastName = "Rocha";
+        const COUNTRY = "Estados Unidos";
+        echo "My name is $name $lastName and i want to know " . COUNTRY;
+
 ?>
